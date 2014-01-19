64cddbea3b7c085570c193292336ce57141a5e79
wsnbbdbd <qi_weigang@hotmail.com>
2014-01-19 17:51:25 +0800

    Revert "hehelife"
    This reverts commit 74906848cd3bf76413e20df6d313aa2b003d88a1.

--- code/admin/protected/modules/test/controllers/DefaultController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class DefaultController extends CController {
-	
-	public function actionIndex(){
-		exit('i\'m test！' );
-	}
-}
--- code/admin/protected/yiic.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/library/Yii/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
-
-require_once($yiic);
-
-?>
